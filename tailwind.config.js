69a01418dcc02bcae432fde8ab95d3ba8c304e59
harymunnli <prgmrharry@gmail.com>
2022-12-30 18:35:15 +0700

    init & short introduction section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 'index.html' ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#d946ef',
+        dark: "#292524"
+      }
+    },
+  },
+  plugins: [],
+}
