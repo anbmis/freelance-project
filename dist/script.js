38b006165f5fc640619550ae493827da30504038
harymunnli <prgmrharry@gmail.com>
2023-01-01 16:37:55 +0700

    navbar half finished

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,8 @@
 // Hamburger
 const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
