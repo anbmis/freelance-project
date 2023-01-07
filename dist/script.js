e27272bdf5a8f8e1270040a3a1790b6fb08caed5
harymunnli <prgmrharry@gmail.com>
2023-01-07 20:15:05 +0700

    perbaiki navbar lagi

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,9 @@
 //     hamburger.classList.toggle('hamburger-active');
 //     navMenu.classList.toggle('hidden');
 // });
+
+const btn = document.getElementById("syapen-btn")
+const syapen = document.getElementById("syapen");
+btn.addEventListener("click", () => {
+    syapen.classList.toggle('hidden');
+})
